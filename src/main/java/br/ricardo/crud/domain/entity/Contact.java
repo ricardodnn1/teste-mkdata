2aa3f484b77750b352e2df4e3293decab9ac504d
Ricardo Nunes <ricardodnn1@gmail.com>
2022-08-21 19:31:57 -0300

    Add PostgreSQL

--- src/main/java/br/ricardo/crud/domain/entity/Contact.java
+++ src/main/java/br/ricardo/crud/domain/entity/Contact.java
@@ -13,7 +13,7 @@ import javax.persistence.Table;
 public class Contact {
     
     @Id
-    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @GeneratedValue(strategy = GenerationType.SEQUENCE)
     private Long id;
 
     @Column(nullable = true)
--- src/main/java/br/ricardo/crud/domain/entity/Customer.java
+++ src/main/java/br/ricardo/crud/domain/entity/Customer.java
@@ -19,7 +19,7 @@ import br.ricardo.crud.domain.dto.CustomerDto;
 public class Customer {
     
     @Id
-    @GeneratedValue(strategy = GenerationType.IDENTITY)  
+    @GeneratedValue(strategy = GenerationType.SEQUENCE)  
     private Long id;
 
     @Column(nullable = false)
