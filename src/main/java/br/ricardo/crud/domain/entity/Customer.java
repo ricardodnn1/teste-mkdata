c42e0f7cd5f55e0edfb2db08e93f4714fd61fd5c
Ricardo Nunes <ricardodnn1@gmail.com>
2022-08-29 12:18:05 -0300

    Date

--- src/main/java/br/ricardo/crud/domain/entity/Customer.java
+++ src/main/java/br/ricardo/crud/domain/entity/Customer.java
@@ -5,6 +5,7 @@ import java.util.List;
 import javax.persistence.CascadeType;
 import javax.persistence.Column;
 import javax.persistence.Entity;
+import javax.persistence.EntityListeners;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
@@ -13,10 +14,13 @@ import javax.persistence.OneToMany;
 import javax.persistence.Table;
 import javax.validation.constraints.NotBlank;
 
+import org.springframework.data.jpa.domain.support.AuditingEntityListener;
+
 import br.ricardo.crud.domain.dto.CustomerDto;
 
 @Entity
 @Table(name="customers")
+@EntityListeners(AuditingEntityListener.class)
 public class Customer  extends BaseEntity {
     
     @Id
